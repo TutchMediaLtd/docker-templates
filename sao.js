9ddb7626ef89ebfcf762818deb825595bbc3012a
Dean Fields <dean@deanfields.co.uk>
2017-03-11 16:23:11 +0000

    Remove filter

--- sao.js
+++ sao.js
@@ -1,15 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-const types = fs.readdirSync(path.join(__dirname, 'template'))
-  .filter(v => /^docker-/.test(v))
-  .map(v => v.substring(7))
-
-const filters = {}
-types.forEach(type => {
-  filters[`docker-${type}`] = `type === "${type}"`
-})
-
 module.exports = {
   enforceCurrentFolder: true,
   prompts: {
@@ -19,5 +10,5 @@ module.exports = {
       choices: types
     }
   },
-  templateFolder: `${answers.type}`
+  templateFolder: "${answers.type}"
 }
